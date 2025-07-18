54a67c652f62a266951bdd4b97ce357f134b7d4d
NaifM8 <iinaif-@hotmail.com>
2025-07-18 22:22:46 +0300

    Done

--- planteer/plants/urls.py
+++ planteer/plants/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
-from .views import plant_list_view, plant_create_view, plant_search_view, plant_detail_view, plant_update_view, plant_delete_view
+from . import views 
 
 app_name = 'plants'
 
 urlpatterns = [
-    path('all/', plant_list_view, name='plant_list'),
-    path('new/', plant_create_view, name='plant_create'),
-    path('search/', plant_search_view, name='search'),
-    path('<int:plant_id>/detail/', plant_detail_view, name='plant_detail'),
-    path('<int:plant_id>/update/', plant_update_view, name='plant_update'),
-    path('<int:plant_id>/delete/', plant_delete_view, name='plant_delete'),
-
-
+    path('all/', views.plant_list_view, name='plant_list'),
+    path('new/', views.plant_create_view, name='plant_create'),
+    path('search/', views.plant_search_view, name='plant_search'),
+    path('<int:plant_id>/detail/', views.plant_detail_view, name='plant_detail'),
+    path('<int:plant_id>/update/', views.plant_update_view, name='plant_update'),
+    path('<int:plant_id>/delete/', views.plant_delete_view, name='plant_delete'),
 ]
+
