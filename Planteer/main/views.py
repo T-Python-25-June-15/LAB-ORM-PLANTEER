451f72e18575e5c15d3dd79f378746e48e151d08
Abdullah Alsofayan <alsofayan10@gmail.com>
2025-07-19 18:51:54 +0300

    make contact us page, model, forms

--- Planteer/main/views.py
+++ Planteer/main/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from plant.models import Plant
+from .forms import ContactForm
 # Create your views here.
 
 def home_view(request:HttpRequest):
 
     plants = Plant.objects.all()[0:3]
-
-
     return render(request, "home.html", {'plants':plants})
+
+def contact_view(request:HttpRequest):
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("main:contact_view")
+    else:
+        form = ContactForm()
+
+    return render(request, "contact.html", {"form":form})
 
