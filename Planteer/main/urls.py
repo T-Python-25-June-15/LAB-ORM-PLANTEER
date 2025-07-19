13347225d8aeeb9b0c322eb95450d41c8d46044a
AbdullahSofayan <444101428@student.ksu.edu.sa>
2025-07-19 21:47:52 +0300

    bouns is done, contact us page + messages page (contact/messages/)

--- Planteer/main/urls.py
+++ Planteer/main/urls.py
@@ -7,4 +7,7 @@ app_name = "main"
 urlpatterns = [
     path("", views.home_view, name = "home_view"),
     path("contact/", views.contact_view, name = "contact_view"),
+    path("contact/messages/", views.messages_view, name = "messages_view"),
+    path("delete/<message_id>/", views.delete_message_view, name="delete_message_view"),
+
 ]
--- Planteer/main/views.py
+++ Planteer/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from plant.models import Plant
+from .models import Contact
 from .forms import ContactForm
 # Create your views here.
 
@@ -15,10 +16,26 @@ def contact_view(request:HttpRequest):
         form = ContactForm(request.POST)
 
         if form.is_valid():
+            
             form.save()
             return redirect("main:contact_view")
     else:
         form = ContactForm()
 
     return render(request, "contact.html", {"form":form})
+
+
+def messages_view(request:HttpRequest):
+
+    messages = Contact.objects.all()
+
+    return render(request, "messages.html", {"messages":messages})
+
+
+def delete_message_view(request:HttpRequest, message_id:int):
+
+    message = Contact.objects.get(pk=message_id)
+    message.delete()
+
+    return redirect("main:messages_view")
 
