f2e8080685189cc9a0892acf71cbaf0a861d1f9f
Nawaf-Almalki028 <xp.mo91@gmail.com>
2025-07-20 00:43:12 +0300

    done

--- Planteer/main/views.py
+++ Planteer/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse
+from plants.models import Plants
 
 def main_page(request: HttpRequest):
-
-  return render(request, "main/index.html")
+  plant = Plants.objects.all()
+  return render(request, "main/index.html", {"plant":plant})
--- Planteer/plants/models.py
+++ Planteer/plants/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
-# Create your models here.
 class Plants(models.Model):
     name = models.CharField(default="Plant")
     about = models.TextField(default="Soon!")
     used_for = models.TextField(default="Soon!")
-    image = models.ImageField(default="images/default.jpg")
+    image = models.ImageField(upload_to='images/',default="images/default.jpg")
     category = models.CharField(default='Fruit')
     is_editable = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    plant = models.ForeignKey(Plants, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=100)
+    text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
