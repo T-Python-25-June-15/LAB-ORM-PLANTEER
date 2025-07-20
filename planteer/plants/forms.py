1658ec4825496b745c019a0bcd8bd4d875bdda5c
Moh0py <1vipmoh@gmail.com>
2025-07-20 17:19:53 +0300

    Check

--- planteer/plants/forms.py
+++ planteer/plants/forms.py
@@ -5,7 +5,4 @@ class PlantForm(forms.ModelForm):
     class Meta:
         model = Plant
         fields = ['name', 'about', 'used_for', 'image', 'category', 'is_edible']
-        widgets = {
-            'about': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
-            'used_for': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
-        }
+        
